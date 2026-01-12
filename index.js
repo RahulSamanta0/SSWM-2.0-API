41f9983d62b5ec8066d8eb7a89524a85aa24a056
suvranil-debnath <debnathsuvranil@gmail.com>
2026-01-12 12:15:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',               // allow all origins
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false         // MUST be false when origin is '*'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
